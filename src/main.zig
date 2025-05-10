32d365d12b1d8daf2c8803e5c62a53fb8f48aa9b
LeviathanST <phamquochung170505@gmail.com>
2025-05-10 16:22:28 +0700

    main_loop: update workflow

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,32 @@
 const std = @import("std");
-const rpc = @import("rpc.zig");
-const types = @import("type.zig");
+const lsp = @import("lsp.zig");
+
+const types = lsp.types;
+const Server = @import("Server.zig");
+
+const base_protocol = lsp.base_protocol;
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const stdin = std.io.getStdIn().reader();
-    const message = rpc.readMessage(stdin, allocator) catch |err| {
+    const stdout = std.io.getStdOut().writer();
+
+    const transport: base_protocol.TransportOverStdio = .init(stdin, stdout);
+    const message = transport.readMessage(allocator) catch |err| {
         std.log.warn("Error reading message: {}\n", .{err});
         return;
     };
     defer allocator.free(message);
+    std.log.debug("From server: received {s}", .{message});
 
-    const parse = try std.json.parseFromSlice(types.RequestMessage, allocator, message, .{});
+    const parse = try std.json.parseFromSlice(types.Request, allocator, message, .{});
     defer parse.deinit();
 
     if (!parse.value.validate()) {
         return error.InvalidMessage;
     }
+
+    try transport.writeMessage("received request from client!");
 }
 
 comptime {
