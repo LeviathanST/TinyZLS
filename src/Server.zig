8c266df24930db6aec2e72e95effdc8fcf3948c6
LeviathanST <phamquochung170505@gmail.com>
2025-06-19 04:56:12 +0700

    handle dynamic type in req, res in workflow

--- src/Server.zig
+++ src/Server.zig
@@ -1,51 +1,120 @@
 const std = @import("std");
 
-const base_type = @import("base_type.zig");
+const base_type = @import("lsp.zig").base_type;
 const Transport = @import("Transport.zig");
 const ReadError = Transport.ReadError;
 
 const Server = @This();
+const RequestMessage = base_type.RequestMessage(base_type.RequestParams);
 
 transport: Transport,
+status: Status = .uninitialized,
+allocator: std.mem.Allocator,
+_arena: *std.heap.ArenaAllocator,
 
-pub fn onInitialize(self: Server) !void {
+pub const Status = enum {
+    uninitialized,
+    initializing,
+    initialized,
+    shutdown,
+};
+
+pub fn init(transport: Transport, allocator: std.mem.Allocator) !Server {
+    const arena = try allocator.create(std.heap.ArenaAllocator);
+    arena.* = std.heap.ArenaAllocator.init(allocator);
+    return .{
+        .allocator = arena.allocator(),
+        .transport = transport,
+        ._arena = arena,
+    };
+}
+pub fn deinit(self: *Server) void {
+    const alloc = self._arena.child_allocator;
+    self._arena.deinit();
+    alloc.destroy(self._arena);
+    self.transport.deinit();
+}
+
+pub fn onInitialize(self: *Server, params: base_type.InitializeParams) !base_type.InitializeResult {
+    // TODO:
+    _ = params;
+    errdefer self.*.status = .uninitialized;
+
+    if (self.status == .initialized) {
+        std.log.err("The server has been initialized!", .{});
+        return error.InvalidRequest;
+    }
+    if (self.status == .initializing) {
+        std.log.err("The server is initializing!", .{});
+        return error.InvalidRequest;
+    }
+    self.*.status = .initializing;
     const result: base_type.InitializeResult = .{
         .capabilities = .{ .hoverProvider = true },
         .serverInfo = .default(),
     };
-    try self.transport.sendMessage(result);
+    return result;
 }
 
-pub fn loop(self: Server) !void {
-    const message = self.transport.readMessage() catch |err| {
-        std.log.err("Error reading message: {}\n", .{err});
-        return err;
-    };
-    std.log.debug("[Server] received from client: {s}", .{message});
-    if (message.len == 0) {
-        std.log.err("Message is empty with content-length = 0!", .{});
-        return ReadError.MessageEmpty;
+/// Main loop
+pub fn loop(self: *Server) !void {
+    while (self.status != .shutdown) {
+        const message = self.transport.readMessage() catch |err| {
+            std.log.err("Error reading message: {}\n", .{err});
+            return err;
+        };
+        std.log.debug("[Server] received from client: \n{s}", .{message});
+        if (message.len == 0) {
+            std.log.err("Message is empty with content-length = 0!", .{});
+            return ReadError.MessageEmpty;
+        }
+        try self.processRequest(message);
     }
-    try self.processRequest(message);
-    self.transport.arena.deinit(); // Reset memory after reading once
 }
 
-pub fn deinit(self: *Server) void {
-    self.transport.deinit();
-}
-
-pub fn parseRequest(self: Server, message: []const u8) !base_type.RequestMessage {
-    const value = try std.json.parseFromSliceLeaky(
-        base_type.RequestMessage,
-        self.transport.arena.allocator(),
+pub fn parseRequest(self: Server, message: []const u8) !std.json.Parsed(base_type.RequestJSONMessage) {
+    const value = try std.json.parseFromSlice(
+        base_type.RequestJSONMessage,
+        self.allocator,
         message,
         .{},
     );
     return value;
 }
 
-pub fn processRequest(self: Server, message: []const u8) !void {
-    const req = try self.parseRequest(message);
-    if (!std.mem.eql(u8, req.method, "initialize")) return error.InvalidRequest;
-    try self.onInitialize();
+/// Assert in this function ensure `method` existed.
+pub fn sendMessage(self: *Server, comptime method: []const u8, params: base_type.ParamsType(method)) !void {
+    std.debug.assert(@hasField(base_type.RequestParams, method));
+
+    const res = switch (@field(base_type.RequestParams, method)) {
+        .initialize => try self.onInitialize(params),
+        .other => .{},
+    };
+    try self.transport.writeMessage(res);
+}
+pub fn sendMessageWithError(self: *Server, comptime method: []const u8, params: base_type.ParamsType(method), id: base_type.integer) !void {
+    self.sendMessage(method, params) catch |err| {
+        const res_err: base_type.ResponseJSONMessage = .{
+            .id = id,
+            .@"error" = .{
+                .code = switch (err) {
+                    error.InvalidRequest => @intFromEnum(base_type.LSPErrCode.InvalidRequest),
+                    else => unreachable,
+                },
+                .message = @errorName(err),
+                .data = null,
+            },
+        };
+        try self.transport.writeMessage(res_err);
+        return;
+    };
+}
+
+pub fn processRequest(self: *Server, message: []const u8) !void {
+    const rm = try RequestMessage.parseFromSlice(self.allocator, message);
+
+    switch (rm.params) {
+        .other => std.log.err("Catch unknown req", .{}),
+        inline else => |params, method| try self.sendMessageWithError(@tagName(method), params, rm.id),
+    }
 }
--- src/lsp/base_type.zig
+++ src/lsp/base_type.zig
@@ -1,15 +1,27 @@
+/// https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/
 const std = @import("std");
 const json = std.json;
 
 pub const integer = isize;
 const any = json.Value;
+
+pub fn ParamsType(comptime method: []const u8) type {
+    if (!@hasField(RequestParams, method)) return void;
+    return @FieldType(RequestParams, method);
+}
+pub const RequestParams = union(enum) {
+    initialize: InitializeParams,
+    other: OtherMethod,
+};
+
+const OtherMethod = struct {};
+
 pub const LSPErrCode = enum(i32) {
     ServerNotInitialized = -32002,
     InvalidRequest = -32600,
 };
 
-/// https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/
-pub const RequestMessage = struct {
+pub const RequestJSONMessage = struct {
     const Self = @This();
     jsonrpc: []const u8 = "2.0",
     id: integer,
@@ -17,7 +29,7 @@ pub const RequestMessage = struct {
     params: ?any = null,
 };
 
-pub const ResponseMessage = struct {
+pub const ResponseJSONMessage = struct {
     const Self = @This();
     id: integer,
     result: ?any = null,
@@ -30,6 +42,7 @@ pub const ResponseMessage = struct {
     };
 };
 
+pub const InitializeParams = struct {};
 pub const InitializeResult = struct {
     capabilities: ServerCapabilities,
     serverInfo: ServerInfo,
@@ -46,3 +59,40 @@ pub const InitializeResult = struct {
         }
     };
 };
+
+pub fn RequestMessage(comptime Params: type) type {
+    return struct {
+        const Self = @This();
+        jsonrpc: []const u8 = "2.0",
+        id: integer,
+        params: Params,
+
+        pub fn parseFromSlice(alloc: std.mem.Allocator, s: []const u8) !Self {
+            const parsed = try std.json.parseFromSlice(RequestJSONMessage, alloc, s, .{});
+            defer parsed.deinit();
+            const value: RequestJSONMessage = parsed.value;
+
+            var self = try alloc.create(Self);
+            errdefer alloc.destroy(self);
+            self.id = value.id;
+
+            const param_fields = std.meta.fields(Params);
+            inline for (param_fields) |f| {
+                if (std.mem.eql(u8, f.name, value.method)) {
+                    self.params = @unionInit(
+                        Params,
+                        f.name,
+                        (try std.json.parseFromValue(
+                            f.type,
+                            alloc,
+                            value.params.?,
+                            .{},
+                        )).value,
+                    );
+                }
+            }
+            self.*.id = value.id;
+            return self.*;
+        }
+    };
+}
