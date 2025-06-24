c768e0253a9546a4176e55c9b1a27ab3a07439c5
LeviathanST <phamquochung170505@gmail.com>
2025-06-24 20:36:41 +0700

    chore(lsp): change file-level document symbol

--- src/lsp/lsp.zig
+++ src/lsp/lsp.zig
@@ -1,6 +1,5 @@
-/// This module provide all base type in LSP specification
-/// and some extra utils (dynamic type).
-///
+//! This module provide all base type in LSP specification
+//! and some extra utils (dynamic type).
 pub const base_type = @import("base_type.zig");
 
 const message = @import("message.zig");
